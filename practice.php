182cd92ab7e8048d39bfc9eb73d93bb44fc5c97d
名前 <>
2019-10-22 15:35:35 +0000

    php課題提出１

---
+++ practice.php
@@ -0,0 +1,31 @@
+<?php
+$a=3;
+$b=7;
+echo $a+$b;
+echo "\n";
+$array_month= [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月",
+];
+echo $array_month["August"];
+echo "\n";
+$hello="Hello,";
+$name="shimada";
+$world="'s world!";
+echo $hello.$name.$world;
+// echo 'hello php!';
+echo "\n";
+$tech_boost="tech";
+$tech_boost .=" boost";
+echo $tech_boost;
+?>
